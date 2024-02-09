0d0aa22669ad00307bd2c8e9914a7316398259d8
rajamanikandan-r <rmnforever@gmail.com>
2024-02-09 20:04:26 +0530

    perform bulk actions on gpx files

--- scripts/append_HR_to_gpx_with_gpxpy.py
+++ scripts/append_HR_to_gpx_with_gpxpy.py
@@ -3,28 +3,27 @@ import gpxpy.gpx
 from xml.etree.ElementTree import Element, SubElement, tostring
 import sqlite3
 import calendar
+import glob
 
-gpx_file = open("2024-02-05_11_42_31.895_2024-02-05T11_42+0530.gpx", "r")
-gpx = gpxpy.parse(gpx_file)
-
+files = glob.glob("*.gpx")
 db = sqlite3.connect('gadgetbridge')
 cursor = db.cursor()
 
-for track in gpx.tracks:
-  for segment in track.segments:
-    for track_point in segment.points:
-      seconds = calendar.timegm(track_point.time.timetuple())
-      seconds = seconds - (seconds%60)
-      print(track_point.time)
-      print(seconds)
-      row = cursor.execute("select HEART_RATE from hybrid_hractivity_sample where TIMESTAMP = ?", (seconds,)).fetchone()
-		  # Create extension (hr)
-      hr = row[0]
-      extension = Element("gpxtpx:TrackPointExtension", attrib={"xmlns:gpxtpx": "http://www.garmin.com/xmlschemas/TrackPointExtension/v1"})
-      hr_element = SubElement(extension, "gpxtpx:hr")
-      hr_element.text = str(hr)
-      track_point.extensions.append(extension)
+for file in files:
+	gpx_file = open(file, "r")
+	gpx = gpxpy.parse(gpx_file)
 
-# Save the modified GPX file
-with open("modified_workout.gpx", "w") as output_file:
-    output_file.write(gpx.to_xml())
+	for track in gpx.tracks:
+	  for segment in track.segments:
+	    for track_point in segment.points:
+	      seconds = calendar.timegm(track_point.time.timetuple())
+	      seconds = seconds - (seconds%60)
+	      row = cursor.execute("select HEART_RATE from hybrid_hractivity_sample where TIMESTAMP = ?", (seconds,)).fetchone()
+	      hr = row[0]
+	      extension = Element("gpxtpx:TrackPointExtension", attrib={"xmlns:gpxtpx": "http://www.garmin.com/xmlschemas/TrackPointExtension/v1"})
+	      hr_element = SubElement(extension, "gpxtpx:hr")
+	      hr_element.text = str(hr)
+	      track_point.extensions.append(extension)
+
+	with open("mod_" + file, "w") as output_file:
+	    output_file.write(gpx.to_xml())
